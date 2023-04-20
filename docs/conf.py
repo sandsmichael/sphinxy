e74b961b416e976c8625ae5c5737f597b92e1e7e
Michael <michaelesands@gmail.com>
2023-04-20 16:16:11 -0400

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
 autoclass_content = 'both'
 
 
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -54,7 +55,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'furo'
+# html_theme = 'classic'
+
+html_favicon = "_static/favicon.ico"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
