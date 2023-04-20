c78b99948c084b41d7b6d88fa1a51d3671163a49
Michael <michaelesands@gmail.com>
2023-04-20 16:04:04 -0400

    Added docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -8,6 +8,19 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should
+                be returned. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
